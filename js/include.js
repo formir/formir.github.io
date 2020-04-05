4205d2eca7358e9138c3402554ebab9457163f79
Konrad Kubrak <kubrakkonrad@gmail.com>
2020-04-05 12:59:34 +0200

    Add jekyll

---
+++ js/include.js
@@ -0,0 +1,27 @@
+
+/**
+ * Convert a string to HTML entities
+ */
+String.prototype.toHtmlEntities = function() {
+  return this.replace(/./gm, function(s) {
+      return "&#" + s.charCodeAt(0) + ";";
+  });
+};
+
+/**
+* Create string from HTML entities
+*/
+String.fromHtmlEntities = function(string) {
+  return (string+"").replace(/&#\d+;/gm,function(s) {
+      return String.fromCharCode(s.match(/\d+/gm)[0]);
+  })
+};
+
+
+$(document).ready(function() {
+  $('pre code').each(function(i, block) {
+    encode = $(block)[0].innerHTML.trim().toHtmlEntities()
+    $(block).html(encode);
+    hljs.highlightBlock(block);
+  });
+});
